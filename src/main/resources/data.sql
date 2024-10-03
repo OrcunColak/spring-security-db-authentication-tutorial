18f9d19a63942bbc60736d20c1fd30bbac2f1011
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-03 12:01:08 +0300

    fix schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@
 insert into users (email, password)
 values ('orcun@example.com', '$2a$10$8fwn0LUKql6wTzJHO2QoQ.Nd.59eIyFwaucgBJoiZ/T5SqrqNmyBm');
 
-insert into roles (id, authority) values (1, 'ROLE_ADMIN');
-insert into roles (id, authority) values (2, 'ROLE_USER');
+insert into roles (authority) values ('ROLE_ADMIN');
+insert into roles (authority) values ('ROLE_USER');
 
 insert into user_role (user_id, role_id) values (1, 1);
 insert into user_role (user_id, role_id) values (1, 2);
